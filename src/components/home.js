14141e0c7cc02498bc5530747ce436de7c2debce
CinthiaVB <cinthiaveb@gmail.com>
2022-04-17 20:10:12 -0500

    CSS home

--- src/components/home.js
+++ src/components/home.js
@@ -5,24 +5,34 @@ export const homePetworld = () => {
   homeElement.setAttribute('class', 'homePage');
   const homeDiv = `
     <header class="homeHeader">
-      <div id="headerImg">
-      <img id="logo" src="./img/imgLogin/logo.png" alt="logo">
-      </div>
-      <nav class="homeNav">
-        <ul class="homeBar1" >
-          <li class="listNav"><img class="imgIcon" src="./img/icons/home.png"></li>
-          <li class="listNav"><img class="imgIcon" src="./img/icons/cuidados.png">Cuidados</li>
-          <li class="listNav"><img class="imgIcon" src="./img/icons/alimentación.png">Alimentación</li>
-          <li class="listNav"><img class="imgIcon" src="./img/icons/salud.png">Salud</li>
-          <li class="listNav"><img class="imgIcon" src="./img/icons/adopción.png">Adopción</li>
-          <li class="listNav"><img class="imgIcon" src="./img/icons/venta.png">Venta</li>
-        </ul>
-        <ul class="homeBar2">  
-          <li class="listNav" >Mi perfil</li>
-          <li class="listNav">Cerrar Sesión<img class="imgIcon" src="./img/icons/cerrarSesión.png"></li>
-        </ul>
-      </nav>
+      <input id="menuHamb" type="checkbox">
+      <label for="menuHamb">
+        <img class="imgIconBuscar" src="./img/iconsPost/menuHamburguesa.png">
+      </label>
+      <input id="searchIcon" type="search" placeholder="Buscar"> 
     </header>
+    <section>
+      <aside>      
+        <nav class="homeNav">
+          <img id="logoHome" src="./img/imgLogin/logo.png" alt="logo">
+          <ul class="homeBar1" >
+            <li class="listNav"><img class="imgIcon" src="./img/icons/home.png"></li>
+            <li class="listNav"><img class="imgIcon" src="./img/icons/cuidados.png">Cuidados</li>
+            <li class="listNav"><img class="imgIcon" src="./img/icons/alimentación.png">Alimentación</li>
+            <li class="listNav"><img class="imgIcon" src="./img/icons/salud.png">Salud</li>
+            <li class="listNav"><img class="imgIcon" src="./img/icons/adopción.png">Adopción</li>
+            <li class="listNav"><img class="imgIcon" src="./img/icons/venta.png">Venta</li>
+          </ul>
+          <ul class="homeBar2">  
+            <li class="listNav2" >Mi perfil</li>
+            <li class="listNav2"><img class="imgIcon" src="./img/icons/cerrarSesión.png">Cerrar Sesión</li>
+          </ul>
+        </nav>
+      <aside>
+      <div class="containerPost">
+      
+      </div>
+    </section>
     <div>
     <img  id="iconUser"class="iconProfile" >
     <p id="nameUser"></p>
